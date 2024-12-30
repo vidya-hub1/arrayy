227bbe392f26da6df5ce37399e03dfcc2640252a
shashank boddula <vidyaboddula625@gmail.com>
2024-12-30 09:26:41 +0530

    array

---
+++ js.js
@@ -0,0 +1,39 @@
+// 1.let str = "Hello World!";
+// Relace the substring “World!” with “My Dear Friend” ..
+// Write the program for that
+
+let str = "Hello World!";
+let newone = str.replace("World!","My Dear Friend" );
+console.log(newone)
+
+// 2.let a=”Hungry”;
+// Repeat the “Hungry” for 10 times
+// Write the program for that
+
+let a = "Hungry"; 
+let b = a.repeat(10);
+console.log(b)
+
+// 3.let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+// Find the last index of the string “fox”
+
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+let b1 = sentence.indexOf("fox");
+console.log(b1)
+
+
+// 4.let message = "Hello, world!";Find the string starts with “hi”
+// Write code for that
+
+let message = "Hello, world!";
+console.log(message.startsWith("hi"))
+
+
+// 5.let fileName = "document.pdf";Find the string ends with “.pdf”
+// Write code for that
+
+let fileName = "document.pdf";
+console.log(fileName.endsWith(".pdf"))
+
+
+
